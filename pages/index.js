8ac197a9fefbee43c992d97571ae12f2f37e8841
Saia Fonua <saiafonua@users-MacBook-Pro.local>
2021-01-13 20:49:29 -0700

    birthday

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,116 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import React from "react";
+import Confetti from "react-dom-confetti";
+import { css } from "@emotion/css";
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+class Birthday extends React.Component {
+  state = {
+    item: "🎁",
+    confetti: false,
+    hint: "(don't forget your special gift from Hong & Saia)",
+  };
+  openPresent = () => {
+    let presents = [
+      "😺",
+      "🐶",
+      "🎊",
+      "🎈",
+      "🎉",
+      "✨",
+      "🎇",
+      "🎆",
+      "🐇",
+      "🍂",
+      "🎂",
+    ];
+    this.setState({
+      item: presents[Math.floor(Math.random() * Math.floor(presents.length))],
+      confetti: true,
+      hint: "",
+    });
+  };
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+  render() {
+    const config = {
+      angle: 90,
+      spread: 45,
+      startVelocity: 45,
+      elementCount: 50,
+      decay: 0.9,
+    };
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+    return (
+      <main className="bg-indigo-300 h-screen w-screen text-center">
+        <div className="flex flex-col font-fredokaOne">
+          <h1 className="text-5xl">Happy Birthday</h1>
+          <img
+            src="/susan.png"
+            alt="beautiful mama susan"
+            className="object-contain object-center mt-20 animate-bounce"
+          />
+          <h1 className="text-5xl">Susan Turner Stephens!</h1>
+        </div>
+        <div className="text-sm font-fredokaOne">{this.state.hint}</div>
+        <div className={presentContainer}>
+          <Confetti active={this.state.confetti} config={config} />
+          <button className={presentButton} onClick={this.openPresent}>
+            {this.state.item}
+          </button>
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+    );
+  }
 }
+
+export default Birthday;
+
+const presentContainer = css`
+  display: flex;
+  justify-content: center;
+`;
+const presentButton = css`
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  font-size: 50px;
+
+  &:focus {
+    outline: 0;
+  }
+
+  @media only screen and (max-width: 600px) {
+    @keyframes opacity-change {
+      0% {
+        transform: rotate(0deg);
+      }
+      25% {
+        transform: rotate(20deg);
+      }
+      50% {
+        transform: rotate(-20deg);
+      }
+      100% {
+        transform: rotate(0deg);
+      }
+    }
+
+    animation: opacity-change 1s infinite;
+  }
+  &:hover {
+    @keyframes opacity-change {
+      0% {
+        transform: rotate(0deg);
+      }
+      25% {
+        transform: rotate(20deg);
+      }
+      50% {
+        transform: rotate(-20deg);
+      }
+      100% {
+        transform: rotate(0deg);
+      }
+    }
+
+    animation: opacity-change 1s infinite;
+  }
+`;
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        fredokaOne: ['Fredoka One', 'system-ui']
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
